9cfaf3c638c96beca9def3fc8b04d43ef3664afa
Mili_prasad <miliprasad2005@gmail.com>
2024-07-20 19:48:51 +0530

    use-effect-state

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
-import React from "react";
-import {BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
 
-// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
-import "./App.css"
+import React from 'react'
+import UseState from './UseState'
+import UseEffect from './UseEffect';
+
 
 const App = () => {
   return (
     <div>
-      <Router>
-        <Navbar/>
-       
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-};
+      { <UseState/> }
+      { <UseEffect/> }
 
-export default App;
+     
+    </div>
+  )
+}
+
+export default App
