063fff9a02848fa6013602d880c8493235b5ae28
Mili_prasad <miliprasad2005@gmail.com>
2024-07-20 11:08:58 +0530

    context-api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import UseState from './UseState';
-import UseEffect from './UseEffect';
-/*import ToggleFunc from './component/ToggleFunc';*/
+
+import MovieProvider from './DynamicContext.jsx/MovieProvider';
+import MovieList from './DynamicContext.jsx/MovieList';
 
 const App = () => {
   return (
     <div>
-      {<UseState/> }
-      {<UseEffect/> }
 
-     <ToggleFunc/>
+      <h1 style={{background:"blue",color:"black"}}>Here is the example of ContextApi, I have used to make Dark and Light mode Theme</h1>
+ 
+      <br/>
+      <br/>
+      <br/>
+      <h1 style={{background:"blue",color:'black'}}>Here is the example of Dynamic context, I send the the movie data from MovieContext to MovieList, and render the Data's </h1>
+
+      <MovieProvider>
+        <MovieList/>
+      </MovieProvider>
+
+
     </div>
   )
 }
